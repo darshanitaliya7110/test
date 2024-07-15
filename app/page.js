6e05e50892e70aaa09aa6f50d2d2588230c87b8d
darshanitaliya7110 <darshan.italiya@pixer.digital>
2024-07-15 17:17:30 +0530

    add demo

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import Demo from "@/component/Demo";
 import Test from "@/component/Test";
 import Image from "next/image";
 
@@ -5,6 +6,7 @@ export default function Home() {
   return (
     <>
       <Test />
+      <Demo />
     </>
   );
 }
